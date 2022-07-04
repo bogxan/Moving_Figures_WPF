99829bbd778c51c705c0b02ecd99393d50db1d23
bogxan <bshchokin@gmail.com>
2022-07-04 12:43:14 +0300

    Added ukrainian language

--- task1/App.xaml.cs
+++ task1/App.xaml.cs
@@ -18,6 +18,7 @@
             languages.Clear();
             languages.Add(new CultureInfo("en-US"));
             languages.Add(new CultureInfo("ru-RU"));
+            languages.Add(new CultureInfo("uk-UA"));
             Language = task1.Properties.Settings1.Default.DefaultLanguage;
         }
 
@@ -54,14 +55,21 @@
 
                 System.Threading.Thread.CurrentThread.CurrentUICulture = value;
                 ResourceDictionary dict = new ResourceDictionary();
-                switch (value.Name)
+                try
                 {
-                    case "ru-RU":
-                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
-                        break;
-                    default:
+                    if (value.Name == "en-US")
+                    {
                         dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
-                        break;
+                    }
+                    else
+                    {
+                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unsupported language!");
+                    return;
                 }
 
                 ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
